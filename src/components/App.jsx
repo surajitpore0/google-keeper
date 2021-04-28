b86f749dbdc88b6b49ca27bd7babab79070e5729
PoreXD <surajitpore0@gmail.com>
2021-04-28 14:47:13 +0530

    Next

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,11 +3,13 @@ import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import Notes from "../Notes";
+import CreateArea from "./CreateArea";
 
 function App() {
     return (
         <div>
             <Header />
+            <CreateArea />
             {Notes.map((noteItem) => (
                 <Note
                     key={noteItem.key}
